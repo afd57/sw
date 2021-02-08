71f4cc597b6366a416872699f2cd5c7d0627a62c
afd57 <afdagli@gmail.com>
2021-02-08 22:40:31 +0300

    Modified template.

--- src/sw_website/sw_app/views.py
+++ src/sw_website/sw_app/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-
+from sw_app.models import Story
 # Create your views here.
 
 def index(request):
-
+    all_story = Story.objects.all()
     return render(
         request,
         'display.html',
